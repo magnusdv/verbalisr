6bf8bb0586980f91a106f01869d33bb1ff705ebd
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2021-11-03 16:28:07 +0100

    Cleanups for CRAN release

--- R/verbalisr-package.R
+++ R/verbalisr-package.R
@@ -1,7 +1,8 @@
 #' verbalisr: Describe pedigree relationships in words
 #'
 #' Describe the genealogical relationship between two members of a given
-#' pedigree.
+#' pedigree. verbalisr is part of the *ped suite* collection of packages for
+#' pedigree analysis.
 #'
 #' @docType package
 #' @import pedtools
