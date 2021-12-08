1e4506e47a0147307687d32e0548775eb12cd8bc
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2021-12-08 16:23:01 +0100

    Export `format.pairrel()` and add arguments `cap` and `includePaths`

--- R/print.R
+++ R/print.R
@@ -1,5 +1,16 @@
+#' Format relationship description
+#'
+#' This is the main formatting function called by `print.pairrel()`.
+#'
+#' @param x An output of [verbalise()].
+#' @param cap A logical indicating if the first letter of each path description
+#'   should be capitalised. By default TRUE.
+#' @param includePaths A logical indicating if the complete paths should be
+#'   included in the output. By default TRUE.
+#' @param ... Not used.
+#'
 #' @export
-format.pairrel = function(x, ...) {
+format.pairrel = function(x, cap = TRUE, includePaths = TRUE, ...) {
 
   if(length(x) == 1 && x[[1]]$type == "unrelated")
     return("Unrelated")
@@ -8,30 +19,28 @@ format.pairrel = function(x, ...) {
   descrips = vapply(x, FUN.VALUE = "", function(p)
     if(is.null(p$details)) p$rel else paste0(p$rel, ": ", p$details))
 
-  # Add paths
+  # Collect path groups
   uniq = unique.default(descrips)
   paths = lapply(uniq, function(dsc)
     sapply(x[descrips == dsc], function(p) p$path))
-  names(paths) = doublify(uniq, n = lengths(paths))
+
+  s = doublify(uniq, n = lengths(paths))
+  if(cap)
+    s = capit(s)
+
+  names(paths) = s
 
   # Collect and print
-  completeText(paths)
+  if(includePaths)
+    s = unlist(lapply(s, function(r) c(r, paste("  ", paths[[r]]))))
+
+  s
 }
 
 
 #' @export
 print.pairrel = function(x, ...) {
-  txt = format(x)
+  txt = format(x, ...)
   cat(txt, sep = "\n")
   invisible(x)
-}
-
-
-completeText = function(x) {
-  lines = lapply(names(x), function(s) {
-    pths = x[[s]]
-    c(capit(s), paste("  ", pths))
-  })
-
-  unlist(lines)
 }
