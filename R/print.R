3e0798765b2e38a987fc50dd6458f1038ae7544b
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2024-03-17 15:56:55 +0200

    Add formatting argument `simplify`, by default FALSE

--- R/print.R
+++ R/print.R
@@ -5,19 +5,25 @@
 #' @param x An output of [verbalise()].
 #' @param cap A logical indicating if the first letter of each path description
 #'   should be capitalised. By default TRUE.
+#' @param simplify A logical. If TRUE, the descriptions of lineal and avuncular
+#'   relationships are simplified. Default: FALSE.
 #' @param includePaths A logical indicating if the complete paths should be
 #'   included in the output. By default TRUE.
 #' @param ... Not used.
 #'
 #' @export
-format.pairrel = function(x, cap = TRUE, includePaths = TRUE, ...) {
+format.pairrel = function(x, cap = TRUE, simplify = FALSE,
+                          includePaths = !simplify, ...) {
 
   if(length(x) == 1 && x[[1]]$type == "unrelated")
     return("Unrelated")
 
   # Descriptions: Relationship + details
-  descrips = vapply(x, FUN.VALUE = "", function(p)
-    if(is.null(p$details)) p$rel else paste0(p$rel, ": ", p$details))
+  descrips = vapply(x, FUN.VALUE = "", function(p) {
+    if(simplify && !is.null(p$details2)) p$details2
+    else if(is.null(p$details)) p$rel
+    else paste0(p$rel, ": ", p$details)
+  })
 
   # Collect path groups
   uniq = unique.default(descrips)
